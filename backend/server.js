422ed0042337d14f5960eb8c7b182dcacb10afeb
Kanykei Makishova <kanykeimakishova@MacBook-Pro.local>
2020-11-04 16:34:38 -0600

    fixstart server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require ('express')
 const dishes = require('./dishes.js')
 const server = express()
-
+require('dotenv').config()
+const PORT=process.env.NODE_ENV || 8000
 server.get('/api/dishes',(req,res)=>{
     res.json(dishes)
 })
@@ -14,6 +15,6 @@ server.get('/api/dishes/:id',(req,res)=>{
 if (process.env.NODE_ENV === 'production'){
     server.use(express.static(path.join(path.resolve(), '/frontend/build')))
 }
-server.listen(8000,  ()=> {
+server.listen(PORT,  ()=> {
     console.log(`Server is running. Port: 8000`)
 })
