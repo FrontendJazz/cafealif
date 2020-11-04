fad27727c718f0ee09f6b83be1a8cb9c6e6f9489
Kanykei Makishova <kanykeimakishova@MacBook-Pro.local>
2020-11-04 16:48:03 -0600

    fix index.html

---
+++ frontend/build/precache-manifest.b9255dddd0097b22640f779481c89de1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4224649ecc15578ca8d201c87c02d250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969dcf0be465cf65852a",
+    "url": "/static/css/main.37c3db9c.chunk.css"
+  },
+  {
+    "revision": "785c17f2c3fbf4938048",
+    "url": "/static/js/2.468218b9.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.468218b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969dcf0be465cf65852a",
+    "url": "/static/js/main.1ea7f41f.chunk.js"
+  },
+  {
+    "revision": "559b85bfb1e74791d17f",
+    "url": "/static/js/runtime-main.516a8143.js"
+  },
+  {
+    "revision": "f1e52943540d79f5540f688362aa848c",
+    "url": "/static/media/hero.f1e52943.jpg"
+  }
+]);
